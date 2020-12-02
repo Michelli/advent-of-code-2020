b7a5c4b181236f74a2471ad5b511602ff2c3a0a2
Michelli <Michelli@users.noreply.github.com>
2020-12-02 11:32:31 +0100

    Day 2, part 2: indexing strings

--- 02/program.py
+++ 02/program.py
@@ -13,20 +13,21 @@ def read_input(filename):
     return entries
 
 
-def part_one():
-    entries = read_input("input.txt")
+def part_one(filename):
+    entries = read_input(filename)
 
     # Total valid passwords
     valid_count = 0
 
     for line in entries:
         # Create named groups
-        regex = re.match(r"(?P<min>^\d+)-(?P<max>\d+)\s(?P<letter>[a-z]):\s(?P<password>[a-z]+$)", line)
-        
+        regex = re.match(
+            r"(?P<min>^\d+)-(?P<max>\d+)\s(?P<letter>[a-z]):\s(?P<password>[a-z]+$)", line)
+
         # Variables
+        counter = 0
         min_times = int(regex.group('min'))
         max_times = int(regex.group('max'))
-        counter = 0
 
         # Check password
         for letter in regex.group('password'):
@@ -35,6 +36,35 @@ def part_one():
 
         # Increase counter if password is valid
         if counter >= min_times and counter <= max_times:
+            valid_count += 1
+
+    return valid_count
+
+
+def part_two(filename):
+    entries = read_input(filename)
+
+    # Total valid passwords
+    valid_count = 0
+
+    for line in entries:
+        # Create named groups
+        regex = re.match(
+            r"(?P<first_position>^\d+)-(?P<second_position>\d+)\s(?P<letter>[a-z]):\s(?P<password>[a-z]+$)", line)
+
+        # Variables
+        counter = 0
+        first_index = int(regex.group('first_position')) - 1
+        second_index = int(regex.group('second_position')) - 1
+
+        # Check both positions
+        if regex.group('password')[first_index] == regex.group('letter'):
+            counter += 1
+        if regex.group('password')[second_index] == regex.group('letter'):
+            counter += 1
+
+        # Check if ONLY one position is correct
+        if counter == 1:
             valid_count += 1
 
     return valid_count
