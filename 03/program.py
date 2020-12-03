8a58edf20a9811e9fccd117489eb987839032bcd
Michelli <Michelli@users.noreply.github.com>
2020-12-03 15:13:41 +0100

    Day 3, part 2

--- 03/program.py
+++ 03/program.py
@@ -1,5 +1,6 @@
 import os
 import sys
+from math import prod
 
 
 def read_input(filename):
@@ -36,4 +37,32 @@ def part_one(filename):
 
 
 def part_two(filename):
-    pass
+    entries = read_input(filename)
+
+    # Running count
+    total_trees = []
+
+    for right, down in zip([1, 3, 5, 7, 1], [1, 1, 1, 1, 2]):
+        # Reset counter
+        trees = 0
+
+        # Reset position
+        position = 0
+
+        for row in enumerate(entries[::down]):
+            # Ensure position does not exceed index
+            if (position + 1) > len(row[1]):
+                # Repeat the row
+                position -= len(row[1])
+
+            # Check if it's a tree
+            check = row[1][position]
+            if row[1][position] == "#":
+                trees += 1
+
+            # Move to the next position
+            position += right
+
+        total_trees.append(trees)
+
+    return prod(total_trees)
