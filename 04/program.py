addb80a71eccdc0b7f075b579b71d69403c0cb95
Michelli <Michelli@users.noreply.github.com>
2020-12-04 13:52:28 +0100

    Day 3, part 2: I like regex a bit too much

--- 04/program.py
+++ 04/program.py
@@ -27,3 +27,51 @@ def part_one(filename):
             counter += 1
 
     return counter
+
+
+def part_two(filename):
+    passports = create_entries(filename)
+    valid_passports = 0
+
+    for passport in passports:
+        # Repeat checks of part one
+        value_names = re.findall(r"[a-z]{3}(?=:)", passport)
+
+        # Start second test
+        if len(value_names) == 8 or len(value_names) == 7 and 'cid' not in value_names:
+            # Passport rules passed
+            check_counter = 0
+
+            # Birth year: Between 1920-2002
+            if len(re.findall(r"(?<=byr:)19[2-9]\d|200[0-2]", passport)) > 0:
+                check_counter += 1
+
+            # Eye colour: List of colour codes
+            if len(re.findall(r"(?<=ecl:(amb|blu|brn|gry|grn|hzl|oth))", passport)) > 0:
+                check_counter += 1
+
+            # Expiration year: Between 2020-2030
+            if len(re.findall(r"(?<=eyr:)20(?:(?:2\d)|(?:30))", passport)) > 0:
+                check_counter += 1
+
+            # Hair colour: Colour hex code with leading hashtag
+            if len(re.findall(r"(?<=hcl:)#[0-9a-f]{6}", passport)) > 0:
+                check_counter += 1
+
+            # Height: Between 150-193cm OR 59-76in
+            if len(re.findall(r"(?<=hgt:)1(?:[5-8]\d|9[0-3])cm|(?:59|6\d|7[0-6])in", passport)) > 0:
+                check_counter += 1
+
+            # Issue year: Between 2010-2020
+            if len(re.findall(r"(?<=iyr:)20(?:(?:1\d)|(?:20))", passport)) > 0:
+                check_counter += 1
+
+            # Passport ID: Nine digits
+            if len(re.findall(r"(?<=pid:)\d{9}(?=\s|\n|$)", passport)) > 0:
+                check_counter += 1
+
+            # All checks passed
+            if check_counter == 7:
+                valid_passports += 1
+
+    return valid_passports
